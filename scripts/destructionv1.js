0089c889bcff4843426f8de7ff4886d0f9f2a28a
Julien Ribardiere <julien.ribardiere14@gmail.com>
2025-06-10 00:03:51 +0200

    commit.6

--- scripts/destructionv1.js
+++ scripts/destructionv1.js
@@ -2,7 +2,7 @@ window.addEventListener('DOMContentLoaded', function() {
     const confirmation = confirm("Êtes-vous sûr de vouloir lancer la séquence d'autodestruction ?");
     if (confirmation) {
         // Chemin relatif correct depuis /pages/ vers /scripts/
-        const ambiance = new Audio('../scripts/ytmp3free.cc_alien-nostromo-destruct-sequence-youtubemp3free.org.mp3');
+        const ambiance = new Audio('../scripts/nostromo_destruction.mp3');
         ambiance.loop = true;
         ambiance.play().catch(e => alert("Erreur audio : " + e.message));
     }
